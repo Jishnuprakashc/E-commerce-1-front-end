13b80b42d66864d480b61b1f53ee6b5580a3e2f0
jishnu prakash c <jishnuprakashc7@gmail.com>
2025-04-08 13:29:34 +0530

    changes

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../Hero/Hero'
 import Popular from '../Components/popular/Popular'
-import Offers from '../Components/offers/Offers'
+import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
